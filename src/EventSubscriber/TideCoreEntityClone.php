e58b0a80aacfee416d125ffe9d6eb5502b58f267
Guy Owen <guy.owen@dpc.vic.gov.au>
2020-11-17 17:27:33 +1100

    [SDPA-4755] Cleaned up PostClone ops.

--- src/EventSubscriber/TideCoreEntityClone.php
+++ src/EventSubscriber/TideCoreEntityClone.php
@@ -4,7 +4,7 @@ namespace Drupal\tide_core\EventSubscriber;
 
 use Drupal\entity_clone\Event\EntityCloneEvent;
 use Drupal\entity_clone\Event\EntityCloneEvents;
-use Drupal\user\UserInterface;
+use Drupal\user\Entity\User;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
@@ -15,19 +15,16 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 class TideCoreEntityClone implements EventSubscriberInterface {
 
  public static function getSubscribedEvents() {
-   $a = 'b';
    return [
      EntityCloneEvents::POST_CLONE => 'postCloneUpdate'
    ];
  }
 
  public function postCloneUpdate(EntityCloneEvent $event) {
-   $current_user = \Drupal\user\Entity\User::load(\Drupal::currentUser()
+   $current_user = User::load(\Drupal::currentUser()
      ->id());
    $cloned_entity = $event->getClonedEntity();
-   $a = 'b';
-   $cloned_entity->setRevisionUser($current_user);
-   $user = $cloned_entity->getRevisionUser();
+   $cloned_entity->setOwner($current_user);
    $cloned_entity->save();
  }
 
